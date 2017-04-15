c8fd4102f0fce6504a2f4ca100fe51087a724054
Daniel Koza <dakolech@gmail.com>
2017-04-15 23:13:40 +0200

    add react router

--- src/store.js
+++ src/store.js
@@ -1,6 +1,18 @@
-import { combineReducers } from 'redux';
+import { combineReducers, applyMiddleware, createStore, compose } from 'redux';
 import { photos } from './photos/photos.reducer';
+import { routerReducer, routerMiddleware } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory';
 
-export const storeReducers = combineReducers({
-  photos
-});
+export const history = createHistory();
+const middleware = routerMiddleware(history);
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+export const store = createStore(
+  combineReducers({
+    photos,
+    router: routerReducer
+  }), composeEnhancers(
+    applyMiddleware(middleware)
+  )
+);
