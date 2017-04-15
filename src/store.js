a58cf32813fcc1cdc9bc5bd9c528ed1074caee77
Daniel Koza <dakolech@gmail.com>
2017-04-15 23:26:40 +0200

    add saga

--- src/store.js
+++ src/store.js
@@ -2,9 +2,14 @@ import { combineReducers, applyMiddleware, createStore, compose } from 'redux';
 import { photos } from './photos/photos.reducer';
 import { routerReducer, routerMiddleware } from 'react-router-redux';
 import createHistory from 'history/createBrowserHistory';
+import createSagaMiddleware from 'redux-saga';
+import { fork } from 'redux-saga/effects';
+import { photosSaga } from './photos/photos.saga';
 
 export const history = createHistory();
-const middleware = routerMiddleware(history);
+const sagaMiddleware = createSagaMiddleware();
+
+const middleware = [routerMiddleware(history), sagaMiddleware];
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
@@ -13,6 +18,14 @@ export const store = createStore(
     photos,
     router: routerReducer
   }), composeEnhancers(
-    applyMiddleware(middleware)
+    applyMiddleware(...middleware)
   )
 );
+
+function* rootSaga() {
+  yield [
+    fork(photosSaga),
+  ];
+}
+
+sagaMiddleware.run(rootSaga);
