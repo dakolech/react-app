4039e8c60c1da625e20090e17b956108c7ecd243
Daniel Koza <dakolech@gmail.com>
2017-04-18 15:55:08 +0200

    fix api url

--- src/photos/photos.saga.js
+++ src/photos/photos.saga.js
@@ -6,13 +6,13 @@ import { apiError, savePhotos } from "./photos.actions";
 import { pipe, map, take } from 'ramda';
 
 const API_PARAMS = {
-  key: 'a7ed363fbbbd162d61a3b05eb81b38a3',
+  key: 'bead0153d50ff3255a9367c18e4973a6',
   text: 'MarilynMonroe',
   authToken: '72157682719016735-974a42f13723e292',
-  sig: '87a23e9690115adc1efb919ebc0d0cdc',
+  sig: 'b7f94974b3b340e40d69616cbaf407b5',
 }
 
-export const API_URL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_PARAMS.key}&text=${API_PARAMS.text}&format=json&nojsoncallback=1&auth_token=${API_PARAMS.authToken}&api_sig=${API_PARAMS.sig}`;
+export const API_URL = `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${API_PARAMS.key}&text=${API_PARAMS.text}&format=json&nojsoncallback=1&api_sig=${API_PARAMS.sig}`;
 
 export function* locationChanged({ payload: { pathname }}) {
   if (pathname === PHOTOS_PAGE) {
